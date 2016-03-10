4222485b2cdc2c816df3d6018b42ca0076f450ae
Junnie-Jobs <dj0999@naver.com>
2016-03-10 11:12:16 +0900

    2016.3.10 finish

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -4,68 +4,82 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 
 public class HttpRequestUtils {
 	/**
-	 * @param queryString은 URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
+	 * @param queryString은
+	 *            URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
 	 * @return
 	 */
+
+	private static final Logger log = LoggerFactory.getLogger(HttpRequestUtils.class);
+
+	public static String getUrl(String firstLine) {
+
+		String[] splited = firstLine.split(" ");
+		String path = splited[1];
+		log.debug("request path : {}", path);
+		return path;
+	}
+
 	public static Map<String, String> parseQueryString(String queryString) {
 		return parseValues(queryString, "&");
 	}
-	
+
 	/**
-	 * @param 쿠키 값은 name1=value1; name2=value2 형식임
+	 * @param 쿠키
+	 *            값은 name1=value1; name2=value2 형식임
 	 * @return
 	 */
 	public static Map<String, String> parseCookies(String cookies) {
 		return parseValues(cookies, ";");
 	}
-	
+
 	private static Map<String, String> parseValues(String values, String separator) {
 		if (Strings.isNullOrEmpty(values)) {
 			return Maps.newHashMap();
 		}
-		
+
 		String[] tokens = values.split(separator);
-		return Arrays.stream(tokens)
-					.map(t -> getKeyValue(t, "="))
-					.filter(p -> p != null)
-					.collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
+		return Arrays.stream(tokens).map(t -> getKeyValue(t, "=")).filter(p -> p != null)
+				.collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
 	}
-	
+
 	static Pair getKeyValue(String keyValue, String regex) {
 		if (Strings.isNullOrEmpty(keyValue)) {
 			return null;
 		}
-		
+
 		String[] tokens = keyValue.split(regex);
 		if (tokens.length != 2) {
 			return null;
 		}
-		
+
 		return new Pair(tokens[0], tokens[1]);
 	}
-	
+
 	public static Pair parseHeader(String header) {
 		return getKeyValue(header, ": ");
 	}
-	
+
 	public static class Pair {
 		String key;
 		String value;
-		
+
 		Pair(String key, String value) {
 			this.key = key.trim();
 			this.value = value.trim();
 		}
-		
+
 		public String getKey() {
 			return key;
 		}
-		
+
 		public String getValue() {
 			return value;
 		}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,26 @@
 package webserver;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import db.DataBase;
 import http.HttpRequest;
 import http.HttpResponse;
+import model.User;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,22 +35,42 @@ public class RequestHandler extends Thread {
 		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
 				connection.getPort());
 
-		String uri = "";
+		HttpResponse hr = new HttpResponse(); 
+		HttpRequest hreq = new HttpRequest();
 
-		try {
-			HttpRequest req = new HttpRequest(connection.getInputStream());
-			uri = req.getUri();
-		} catch (IOException error) {
-			log.error(error.getMessage());
-		}
+		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 
-		try {
-			HttpResponse resp = new HttpResponse(connection.getOutputStream());
-			resp.forward(uri);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			String line = br.readLine();
+
+			if (line == null) {
+				return;
+			}
+
+			String url = HttpRequestUtils.getUrl(line);
+			Map<String, String> headers = new HashMap<String, String>();
+
+			line = hreq.getHeader(br, line, headers);
+
+			if (url.startsWith("/user/create")) {
+				hr.createUser(hr, out, br, headers);
+
+			} else if (url.equals("/user/login")) {
+				hr.loginCheck(hr, out, br, headers);
+
+			} else if (url.endsWith(".css")) {
+				hr.cssCheck(hr, out, url);
+
+			}
+
+			else {
+				hr.defaultResponse(hr, out, url);
+
+			}
+
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.error(e.getMessage());
 		}
-
 	}
 
 }
